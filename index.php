35324dae577df07fe7cc6ba377c007c69494b39a
L3DLP <contact@l3dlp.com>
2016-04-23 13:23:47 +0200

    Now ready to use
    + No more subdir for nothing
    + CSS improvements

--- index.php
+++ index.php
@@ -18,21 +18,12 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Uptime Statistics</title>
-    <link rel="stylesheet" href="gfx/style.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-      
+    <link rel="stylesheet" href="gfx/style.css" />
     <style type="text/css">
     body{
         background: url(<?php echo $selectedBg; ?>) no-repeat center center;
@@ -40,21 +31,23 @@
         /* Backgrounds by https://unsplash.com/ `*/
     }
     </style>
-      
-  </head>
-  <body>
+     
+</head>
+<body>
 
-      <div class="transparent">
+    <div class="transparent">
         <div class="field">
             <center>
-		<img src="<?php echo $config['logo']; ?>" class="logo" />
-		<br/>
-		<small>UPTIME STATISTICS
+				<img src="<?php echo $config['logo']; ?>" class="logo" />
+				<br/>
+				<small>UPTIME STATISTICS
             </center>
-            <h1></h1>
-        <?php 
-        //var_dump($results);
-        $print = getOverall($results); 
+            <br/>
+
+<?php 
+
+//var_dump($results);
+$print = getOverall($results); 
 
 
 // 24 Hour Ratio
@@ -73,14 +66,15 @@
         echo "<h4 class='positive' style='margin-top:30px'>".$print['alltime']."%</h4> 
             <span class='ratio-title'><p>from the start</p></span>";
 
-        ?>
+?>
+
         </div>
     </div>
       
-        <div id="footer" class="transparent">
-            <center>Servers are monitored by<a href="https://uptimerobot.com/" target="_blank" class="uptime-logo"><img src="gfx/uptime-logo.png" border="0" /></a><small>Code used here is on <a href="https://github.com/l3dlp/PHP-UptimeRobot-Dashboard" target="_blank">GitHub</a>.<br/>Nice backgrounds come from <a href="https://unsplash.com/" target="_blank">Unsplash</a></small>
-            </center>
-        </div>
+	<div id="footer" class="transparent">
+		<center>Servers are monitored by<a href="https://uptimerobot.com/" target="_blank" class="uptime-logo"><img src="gfx/uptime-logo.png" border="0" /></a><small>Code used here is on <a href="https://github.com/l3dlp/PHP-UptimeRobot-Dashboard" target="_blank">GitHub</a>.<br/>Nice backgrounds come from <a href="https://unsplash.com/" target="_blank">Unsplash</a></small>
+		</center>
+	</div>
 
-            </body>
+</body>
 </html>
